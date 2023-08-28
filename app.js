3cc95d7d0d60c82f5f64e29f48e5832d8e1ebe65
abdmud <amohamud92@hotmail.com>
2023-08-28 11:26:15 +0100

    mobile menu functionality

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const hMenu = document.querySelector(".h-menu");
+const closeIcon = document.querySelector(".icon-menu-close");
+const mobileMenu = document.querySelector(".menu-items");
 
+// Window resize after page refreshes
 function toggle() {    
     if (window.innerWidth < 701) 
     {
         navLinks.classList.add('hidden');
         hamburger.classList.remove('hidden');
-        console.log("non")
+        
     }
 }
 
 toggle();
 
+// hide hamburger icon after click
 hamburger.addEventListener("click", () => {
     hMenu.classList.remove("hidden");
     hamburger.classList.add('hidden');
+    mobileMenu.classList.remove("hidden");
+})
+
+// close mobile list
+closeIcon.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+    hamburger.classList.remove('hidden');
 })
